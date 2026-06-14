09b7f0757a2de2746ac13bdfbd7ae21184713a50
priya05122000 <npriyanagendran2000@gmail.com>
2026-06-14 15:07:01 +0530

    fixes

--- src/app/(booking)/page.tsx
+++ src/app/(booking)/page.tsx
@@ -1,24 +1,3 @@
-// import Image from 'next/image'
-// import SearchSection from '../components/booking/search/SearchSection'
-// import Section from '../components/common/Section'
-
-// const BookingPage = () => {
-//     return (
-//         <Section>
-//             <div className='my-20'>
-//                 <Image
-//                     src="/images/Rectangle.png"
-//                     alt='banner'
-//                     width={1600}
-//                     height={900}
-//                     className='w-full h-64 object-cover'
-//                 />
-//             </div>
-
-
-//             <SearchSection />
-//         </Section>
-//     )
-// }
-
-// export default BookingPage
+export default function Page() {
+    return <div>Home Page</div>;
+}
