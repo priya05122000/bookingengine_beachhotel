dc79b60e0034b807086c5989384d6c2698689e88
04-AKILESH <akileshnagarajan123@gmail.com>
2026-06-25 10:25:20 +0530

    enter on otp

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -5,7 +5,7 @@ import ProfileView from './_components/ProfileView'
 const ProfilePage = () => {
   return (
     <Section>
-      <div className='mx-6 sm:mx-0 mt-14 sm:my-20'>
+      <div className='mx-6 sm:mx-0 mt-24 my-10 sm:my-20'>
 
         <ProfileView />
 
